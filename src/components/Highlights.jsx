81e9aa8397ce3c21d7e6321e561d8a7a822d099a
kayleesatch <kayleesatch@gmail.com>
2024-12-11 19:21:58 -0700

    Finished Video

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -16,24 +16,16 @@ const Highlights = () => {
                         title="Easy and Quick"
                         para="Get access to the book you purchased online instanly." 
                         />
-                        <div className="highlight">
-                            <div className="highlight__img">
-                                <FontAwesomeIcon icon="book-open" />
-                            </div>
-                            <h3 className="highlight__subtitle">10,000+ Books</h3>
-                            <p className="highlight__para">
-                                Library has books in all your favorite categories.
-                            </p>
-                        </div>
-                        <div className="highlight">
-                            <div className="highlight__img">
-                                <FontAwesomeIcon icon="tags" />
-                            </div>
-                            <h3 className="highlight__subtitle">Affordable</h3>
-                            <p className="highlight__para">
-                                Get you hands on popular books for as little as $10.
-                            </p>
-                        </div>
+                        <Highlight 
+                        icon={<FontAwesomeIcon icon="book-open" />}
+                        title="10,000+ Books"
+                        para="Library has books in all your favorite categories."
+                        />
+                        <Highlight
+                        icon={<FontAwesomeIcon icon="tags" />}
+                        title="Affordable"
+                        para="Get your hands on popular books for as little as $10."
+                        />
                     </div>
                 </div>
             </div>
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
+import EmptyCart from "../assets/empty_cart.svg"
+import { Link } from 'react-router-dom/cjs/react-router-dom'
 
-const Cart = ({ cart, changeQuantity }) => {
+const Cart = ({ cart, changeQuantity, removeItem }) => {
     const total = () => {
         let price = 0
         cart.forEach((item) => {
@@ -42,7 +44,7 @@ const Cart = ({ cart, changeQuantity }) => {
                                     <span className="cart__book--price">
                                         {(book.salePrice || book.originalPrice).toFixed(2)}
                                     </span>
-                                    <button className="cart__book--remove">
+                                    <button className="cart__book--remove" onClick={() => removeItem(book)}>
                                         Remove
                                     </button>
                                 </div>
@@ -64,8 +66,18 @@ const Cart = ({ cart, changeQuantity }) => {
                             )}) 
                         }
                     </div>
+                    {
+                        cart.length === 0 && (
+                    <div className="cart__empty">
+                        <img src={EmptyCart} alt="" className="cart__empty--img" />
+                        <h2>You don't have any books in your cart!</h2>
+                        <Link to="/books">
+                        <button className="btn">Browse books</button>
+                        </Link>
+                    </div>
+                        )}
                 </div>
-                <div className="total">
+                {cart.length > 0 && (<div className="total">
                     <div className="total__item total__sub--total">
                         <span>Subtotal</span>
                         <span>${(total() * 0.9).toFixed(2)}</span>
@@ -82,7 +94,7 @@ const Cart = ({ cart, changeQuantity }) => {
                     onClick={() => alert(`Haven't got around to doing this :(`)}>
                         Proceed to checkout
                     </button>
-                </div>
+                </div>)}
             </div>
         </div>
     </div>
